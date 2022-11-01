6141c31b0202870d5edb6d0bd022476e82f19390
Oieusouopi <rafasc866@gmail.com>
2022-10-31 23:32:42 -0300

    styles and page register

--- src/components/cardRegister/CardRegister.jsx
+++ src/components/cardRegister/CardRegister.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react"
+import "./style.css"
 
 export default function CardRegister() {
   return (
-    <div>CardRegister</div>
+    <div>
+      <h1 className="title">Community Code</h1>
+      <div className="input_format">
+        <label className="input_form">
+          Nome
+          <input type="text" />
+        </label>
+        <label className="input_form">
+          Email
+          <input type="email" />
+        </label>
+        <label className="input_form">
+          Senha
+          <input type="password" />
+        </label>
+        <label className="input_form">
+          Senha novamente
+          <input type="password" />
+        </label>
+        <label className="input_form">
+          Apelido
+          <input type="text" />
+        </label>
+          <button type="button">Registrar</button>
+      </div>
+    </div>
   )
 }
