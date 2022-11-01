8ed61abff072f207c04776f775afdc45682bbcdd
Oieusouopi <rafasc866@gmail.com>
2022-11-01 00:10:26 -0300

    logica CardRegister

--- src/components/cardRegister/CardRegister.jsx
+++ src/components/cardRegister/CardRegister.jsx
@@ -1,32 +1,62 @@
-import React from "react"
+import { useState } from "react"
 import "./style.css"
 
 export default function CardRegister() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [nickName, setNickName] = useState("")
+  const [passwordAgain, setPasswordAgain] = useState("")
+
   return (
     <div>
       <h1 className="title">Community Code</h1>
       <div className="input_format">
         <label className="input_form">
           Nome
-          <input type="text" />
+          <input
+            type="text"
+            value={ name }
+            onChange={ ({ target }) => setName(target.value)}
+          />
         </label>
         <label className="input_form">
           Email
-          <input type="email" />
+          <input
+            type="email"
+            value={ email }
+            onChange={ ({ target }) => setEmail(target.value)}
+          />
         </label>
         <label className="input_form">
           Senha
-          <input type="password" />
+          <input
+            type="password"
+            value={ password }
+            onChange={ ({ target }) => setPassword(target.value)}
+          />
         </label>
         <label className="input_form">
           Senha novamente
-          <input type="password" />
+          <input
+            type="password"
+            value={ passwordAgain }
+            onChange={ ({ target }) => setPasswordAgain(target.value)}
+          />
         </label>
         <label className="input_form">
           Apelido
-          <input type="text" />
+          <input
+            type="text"
+            value={ nickName }
+            onChange={ ({ target }) => setNickName(target.value)}
+          />
         </label>
-          <button type="button">Registrar</button>
+          <button
+            type="button"
+          >
+            Registrar
+          </button>
       </div>
     </div>
   )
