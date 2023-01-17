ee06d2ecccd1fd93c32d386464f3e966e207c82b
Oieusouopi <rafasc866@gmail.com>
2023-01-17 12:48:00 -0300

    feat: add value email and password

--- src/components/CardLogin/CardLogin.jsx
+++ src/components/CardLogin/CardLogin.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function CardLogin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>LoginRegister</div>
+    <>
+      <div>
+        <div>
+          <input
+            type="email"
+            placeholder="Digite seu email"
+            onChange={ ({target}) => setEmail(target.value) }
+            value = { email }
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Digite sua senha"
+            onChange={ ({target}) => setPassword(target.value) }
+            value = { password }
+          />
+        </div>
+        <div>
+          <button
+            type="button"
+          >
+            Log in
+          </button>
+        </div>
+      </div>
+    </>
   )
 }
